37dbe97e14f7aabe6b30ed53617a99012d69360b
leeeungyo <silverb@infotech.co.kr>
2024-08-29 09:56:52 +0900

    오류수정

--- src/main/java/com/example/secondProject/controller/ArticleController.java
+++ src/main/java/com/example/secondProject/controller/ArticleController.java
@@ -1,12 +1,10 @@
 package com.example.secondProject.controller;
 
 import com.example.secondProject.dto.ArticleForm;
-import com.example.secondProject.dto.myBatis.CommentDtoM;
+import com.example.secondProject.dto.CommentDto;
 import com.example.secondProject.entity.Article;
 import com.example.secondProject.repository.ArticleRepository;
 import com.example.secondProject.service.CommentService;
-import com.example.secondProject.service.myBatis.ArticleServiceM;
-import com.example.secondProject.service.myBatis.CommentServiceM;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
@@ -27,10 +24,6 @@ public class ArticleController {
     private ArticleRepository articleRepository;
     @Autowired
     private CommentService commentService;
-    @Autowired
-    private ArticleServiceM articleServiceM;
-    @Autowired
-    private CommentServiceM commentServiceM;
 
     @GetMapping("/nxweb")
     public String nxWeb(){
@@ -43,123 +36,59 @@ public class ArticleController {
     }
 
     @PostMapping("/articles/create")
-    public String create(ArticleForm form){
-        int maxVal = articleServiceM.nextVal_articleId();
-        int saved = articleServiceM.create(form);
-        return "redirect:/articles/" + maxVal;
+    public String createArticle(ArticleForm form){
+        Article article = form.toEntity();
+        Article saved = articleRepository.save(article);
+        return "redirect:/articles/" + saved.getId();
     }
-
-    // jpa 방식
-//    @PostMapping("/articles/create")
-//    public String createArticle(ArticleForm form){
-//        Article article = form.toEntity();
-//        Article saved = articleRepository.save(article);
-//        return "redirect:/articles/" + saved.getId();
-//    }
 
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id, Model model){
-        Article article = articleServiceM.show2(id);
-        List<CommentDtoM> commentsDtos = commentServiceM.comments(id);
-        model.addAttribute("article", article);
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        List<CommentDto> commentsDtos = commentService.comments(id);
+        model.addAttribute("article", articleEntity);
         model.addAttribute("commentDtos", commentsDtos);
-        log.info("commentsDtos : "+commentsDtos);
         return "articles/show";
     }
 
-    // JPA사용 방식
-//    @GetMapping("/articles/{id}")
-//    public String show(@PathVariable Long id, Model model){
-//        Article articleEntity = articleRepository.findById(id).orElse(null);
-//        List<CommentDto> commentsDtos = commentService.comments(id);
-//        model.addAttribute("article", articleEntity);
-//        model.addAttribute("commentDtos", commentsDtos);
-//        return "articles/show";
-//    }
-
     @GetMapping("/articles")
     public String index(Model model){
-        ArrayList<HashMap<String, Object>> articleList = articleServiceM.index();
-        model.addAttribute("articleEntityList", articleList);
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleEntityList", articleEntityList);
         return "articles/index";
     }
 
-    //JPA방식
-//    @GetMapping("/articles")
-//    public String index(Model model){
-//        ArrayList<Article> articleEntityList = articleRepository.findAll();
-//        model.addAttribute("articleEntityList", articleEntityList);
-//        return "articles/index";
-//    }
-
     @GetMapping("/articles/{id}/edit")
     public String edit(@PathVariable Long id, Model model){
-        Article article = articleServiceM.show2(id);
-        model.addAttribute("article", article);
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
         return "articles/edit";
     }
 
-    // jpa방식
-//    @GetMapping("/articles/{id}/edit")
-//    public String edit(@PathVariable Long id, Model model){
-//        Article articleEntity = articleRepository.findById(id).orElse(null);
-//        model.addAttribute("article", articleEntity);
-//        return "articles/edit";
-//    }
-
     @PostMapping("/articles/update")
-    public String update(ArticleForm article){
-        Article target = articleServiceM.show2(article.getId());
-
+    public String update(ArticleForm form){
+        Article articleEntity = form.toEntity();
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
         if(target != null){
-
-            articleServiceM.update(article);
+            articleRepository.save(articleEntity);      // 엔티티를 DB에 저장(갱신)
         }
-        return "redirect:/articles/"+target.getId();
+        return "redirect:/articles/"+articleEntity.getId();
     }
-
-    // jpa 방식
-//    @PostMapping("/articles/update")
-//    public String update(ArticleForm form){
-//        Article articleEntity = form.toEntity();
-//        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
-//        if(target != null){
-//            articleRepository.save(articleEntity);      // 엔티티를 DB에 저장(갱신)
-//        }
-//        return "redirect:/articles/"+articleEntity.getId();
-//    }
 
     @GetMapping("/articles/{id}/delete")
     public String delete(@PathVariable Long id, RedirectAttributes rttr){
-        Article target = articleServiceM.show2(id);
+        Article target = articleRepository.findById(id).orElse(null);
         if(target != null){
-            List<CommentDtoM> comments = commentServiceM.comments(id);
+            List<CommentDto> comments = commentService.comments(id);
             if(comments.size() > 0){
                 for(int i=0; i<comments.size(); i++){
-                    CommentDtoM commentDtoM = comments.get(i);
-                    commentServiceM.delete(commentDtoM.getId());
+                    CommentDto commentDto = comments.get(i);
+                    commentService.delete(commentDto.getId());
                 }
             }
-            articleServiceM.delete(id);
+            articleRepository.deleteById(id);
             rttr.addFlashAttribute("msg", "삭제됐습니다.");
         }
         return "redirect:/articles";
     }
-    // jpa방식
-//    @GetMapping("/articles/{id}/delete")
-//    public String delete(@PathVariable Long id, RedirectAttributes rttr){
-//        Article target = articleRepository.findById(id).orElse(null);
-//        if(target != null){
-//            List<CommentDtoM> comments = commentService.comments(id);
-//            if(comments.size() > 0){
-//                for(int i=0; i<comments.size(); i++){
-//                    CommentDtoM commentDtoM = comments.get(i);
-//                    commentService.delete(commentDtoM.getId());
-//                }
-//            }
-//            articleRepository.deleteById(id);
-//            rttr.addFlashAttribute("msg", "삭제됐습니다.");
-//        }
-//        return "redirect:/articles";
-//    }
 }
