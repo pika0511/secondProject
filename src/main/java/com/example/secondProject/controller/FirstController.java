7225fd7c7673f2292253a257d4d09a9d5040e555
leeeungyo <silverb@infotech.co.kr>
2024-08-30 09:55:56 +0900

    - thymeleaf 변환중..

--- src/main/java/com/example/secondProject/controller/FirstController.java
+++ src/main/java/com/example/secondProject/controller/FirstController.java
@@ -13,6 +13,26 @@ public class FirstController {
         return "greetings";
     }
 
+    @GetMapping("/hi2")
+    public String niceToMeetYou2(Model model){
+        model.addAttribute("username", "SB");
+        return "hello";
+    }
 
+    @GetMapping("/hi3")
+    public String niceToMeetYou3(Model model){
+        model.addAttribute("username", "SB");
+//        ArrayList<Article> articleEntityList = articleRepository.findAll();
+//        model.addAttribute("articleEntityList", articleEntityList);
+        return "test/hi";
+    }
+
+    @GetMapping("/hello")
+    public String niceToMeetYou4(Model model){
+        model.addAttribute("username", "SB");
+//        ArrayList<Article> articleEntityList = articleRepository.findAll();
+//        model.addAttribute("articleEntityList", articleEntityList);
+        return "articles/test.html";
+    }
 
 }
